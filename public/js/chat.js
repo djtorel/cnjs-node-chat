2747960fafe483faf51f1ea3950346771ce6c1d5
Dominic Torruellas <dtorruellas@gmail.com>
2017-07-06 16:56:03 -0700

    Send messages only to people in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -13,7 +13,7 @@ function scrollToBottom() {
   const lastMessageHeight = newMessage.prev().innerHeight()
 
   if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
-   messages.scrollTop(scrollHeight)
+    messages.scrollTop(scrollHeight)
   }
 }
 
@@ -75,7 +75,6 @@ jQuery('#message-form').on('submit', function (e) {
   const messageTextBox = jQuery('[name=message]')
 
   socket.emit('createMessage', {
-    from: 'User',
     text: messageTextBox.val(),
   }, function () {
     messageTextBox.val('')
